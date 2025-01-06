e0ff2a2d7f97ed07d2d45bf1ef3523b0129bc7db
hyun1010 <rcohyun1010@gmail.com>
2025-01-06 22:30:38 +0900

    mod: cjs, es 방식 모두 build 되도록 수정

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,45 @@
-import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
+const packageJson = require('./package.json');
 
 export default [
   {
     input: 'src/index.ts',
-    output: {
-      file: 'dist/index.js',
-      format: 'esm',
-      globals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
       },
-    },
-    external: ['react', 'react-dom'],
+      {
+        file: packageJson.module,
+        format: 'es',
+        sourcemap: true,
+        exports: 'named',
+      },
+    ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript(),
-      babel({
-        exclude: 'node_modules/**',
-        presets: ['@babel/preset-env', '@babel/preset-react'],
+      typescript({ declaration: true, declarationDir: 'dist/types' }),
+      postcss({
+        modules: true,
+        use: ['sass'],
       }),
       terser(),
     ],
+    // dir을 지정하지 않으면 declarationDir을 제대로 처리할 수 없어서 에러 발생
+    output: [{ dir: 'dist', format: 'es' }],
   },
   {
     input: 'dist/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [
-      dts(), // 타입스크립트 타입 정의를 번들링 해줍니다.
-    ],
+    plugins: [dts()],
   },
 ];
