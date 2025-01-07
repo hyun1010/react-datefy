b781e02fe44187e6202c1be2d9e921d54585365b
ChyunKim <chkim@realsn.com>
2025-01-07 09:48:02 +0900

    mod: 불필요한 import 제거

--- src/internal/ui/input/index.tsx
+++ src/internal/ui/input/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './input.module.scss';
 import { ValueType } from '../../../shared/config';
 import { useDisplayContext } from '../../context/DisplayContext';
